263f67798d858ad6140babfeb441d6259ebc175d
tanzhuqing <tanzhuqing123@163.com>
2013-08-28 16:23:10 +0800

    准确率和召回率

--- learn_recommendations.py
+++ learn_recommendations.py
@@ -7,3 +7,16 @@ def RMSE(records):
 
 def MAE(records):
     return sum([abs(rui-pui) for u,i,rui,pui in records])/float(len(records))
+
+
+#准确率和召回率
+
+def PrecisionRecall(test,N):
+    hit = 0
+    n_recall = 0
+    n_precision = 0
+    for user,items in test.items():
+        rank = Recommend(user,N)
+        hit += len(rank & item)
+        n_precision += N
+    return [hit / (1.0 * n_recall),hit /(1.0*n_precision)]
