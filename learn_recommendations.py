55a6c67fbb53f66276b62d163cdba0be3ca73356
tanzhuqing <tanzhuqing123@163.com>
2013-08-28 16:25:58 +0800

    尼基系数

--- learn_recommendations.py
+++ learn_recommendations.py
@@ -20,3 +20,13 @@ def PrecisionRecall(test,N):
         hit += len(rank & item)
         n_precision += N
     return [hit / (1.0 * n_recall),hit /(1.0*n_precision)]
+
+
+#Äá»ùÏµÊý
+def GiniIndex(p):
+    j = 1
+    n = len(p)
+    G = 0
+    for item,weight in sorted(p.items(),key=itemgetter(1)):
+        G += (2*j -n -1)*weight
+    return G / float(n-1)
