0d972148c43a352fb92ccd8833a8e9d3a7ec298a
tanzhuqing <tanzhuqing123@163.com>
2013-08-28 18:46:33 +0800

    召回率和准确率

--- learn_recommendations.py
+++ learn_recommendations.py
@@ -45,4 +45,30 @@ def SplitData(data,M,k,seed):
         return train,test
 
     
-    
+
+#召回率
+def Recall(train,test,N):
+    hit = 0
+    all = 0
+    for user in train.keys():
+        tu = test[user]
+        rank = GetRecommendation(user,N)
+        for item,pui in rank
+            if item in tu:
+                hit += 1
+        all += len(tu)
+    return hit/(all*1.0)
+
+#准确率
+
+def Precision(train,test,N):
+    hit = 0
+    all = 0
+    for user in train.keys():
+        tu = test[user]
+        rank = GetRecommendation(user,N)
+        for item,pui in rank:
+            if item in tu:
+                hit += 1
+        all += N
+    return hit / (all * 1.0)
